6a2ca0862e8f696fd19823ac3413bd86971031a9
Torrie Fischer <tdfischer@hackerbots.net>
2016-06-14 19:18:22 -0700

    update with some blobby anims for the noisebridge entrance

--- chimes.ino
+++ chimes.ino
@@ -1,35 +1,87 @@
 #include <FastLED.h>
 
-#define LED_NUM 30
-#define CHIME_COUNT 3
+#define CHIME_COUNT 10
+#define CHIME_LENGTH 14
+#define BLOB_COUNT 6
+#define LED_NUM (CHIME_COUNT * CHIME_LENGTH)
 
-class Chime {
-  int m_pos;
-  int m_speed;
-  int m_hue;
-  const int m_fullLength;
-  
+CRGB leds[LED_NUM];
+
+class Blob {
+  uint16_t m_pos;
+  uint16_t m_hue;
+  int16_t m_brightness;
+  int8_t m_fadeDir;
+  int8_t m_velocity;
 public:
+  Blob()
+    : m_pos(0),
+      m_velocity(1),
+      m_hue(0),
+      m_brightness(1),
+      m_fadeDir(1) {}
+      
+  void setPos(uint16_t p) {
+    m_pos = p;
+    m_brightness = p % 120 + 1;
+  }
+
+  void setHue(uint8_t p) {
+    m_hue = p;
+  }
+
+  void setBrightness(uint8_t p) {
+    m_brightness = p;
+  }
+
+  void setVelocity(int8_t v) {
+    m_velocity = v;
+  }
+
+  void step() {
+      m_pos += m_velocity;
+      m_pos %= LED_NUM;
+      m_hue += 1;
+      m_brightness += m_fadeDir;
+      if (m_brightness >= 255 || m_brightness <= 0) {
+        m_fadeDir *= -1;
+      }
+  }
+  
+  void setLEDs(CRGB* leds) {
+      const uint8_t width = 10;
+      for(uint8_t i = 0;i < width; i++) {
+        int absPos = LED_NUM - (m_pos + i - width / 2);
+        if (absPos < 0 || absPos >= LED_NUM)
+          continue;
+        leds[absPos] = blend(leds[absPos], CHSV(m_hue, 200, quadwave8((i / (double)width) * m_brightness)), 128);
+      }
+  }
+};
+
+template<int ChimeLength> class Chime {
+  uint16_t m_pos;
+  uint8_t m_speed;
+  uint16_t m_hue;
+    
+public:
+
+  static const int Length = ChimeLength;
   
   Chime()
     : m_pos(0),
-      m_hue(0),
       m_speed(1),
-      m_fullLength(10) {}
-      
-  int length() const {
-    return m_fullLength;
-  }
+      m_hue(0) {}
   
-  void setSpeed(int i) {
+  void setSpeed(uint8_t i) {
     m_speed = i % 255;
   }
   
-  void setPos(int i) {
+  void setPos(uint8_t i) {
     m_pos = i;
   }
   
-  void setHue(int i) {
+  void setHue(uint8_t i) {
     m_hue = i % 255;
   }
   
@@ -40,7 +92,7 @@ public:
       m_pos += 2;
     }
     
-    if (m_pos > m_fullLength * 40) {
+    if (m_pos > ChimeLength * 20) {
       m_pos = 0;
       m_hue += 3;
       m_hue %= 255;
@@ -48,30 +100,39 @@ public:
   }
   
   void setLEDs(CRGB* leds) {
-    for(int i = 0; i < m_fullLength; i++) {
+    for(int i = 0; i < ChimeLength; i++) {
       if (i > m_pos) {
         leds[i] = CHSV(0, 0, 0);
       } else {
-        double distance = m_pos - i;
-        double brightness = sin((m_fullLength / distance) * (M_PI/2));
+        uint8_t distance = m_pos - i;
+        uint8_t brightness = quadwave8((ChimeLength / (double)distance) * 255);
         if (brightness <= 0.2)
           brightness = 0;
-        leds[i] = CHSV(m_hue, max(200, brightness * 255), brightness * 255);
+        leds[i] = CHSV(m_hue, max(200, brightness), brightness);
       }
     }
   }
 };
 
-Chime chimes[3];
-
-CRGB leds[LED_NUM];
+Chime<CHIME_LENGTH> chimes[CHIME_COUNT];
+Blob blobs[BLOB_COUNT];
 
 void setup() {
   FastLED.addLeds<LPD8806>(leds, LED_NUM);
   for(int i = 0; i < CHIME_COUNT;i++) {
-    chimes[i].setPos(random(chimes[i].length() * 5));
+    chimes[i].setPos(random(Chime<CHIME_LENGTH>::Length * 5));
     chimes[i].setHue(random(255));
-    chimes[i].setSpeed(random(255));
+    chimes[i].setSpeed(random(90) + 128);
+  }k
+  for(int i = 0; i < BLOB_COUNT;i++) {
+    blobs[i].setPos(random(LED_NUM));
+    blobs[i].setHue(random(255));
+    blobs[i].setBrightness(random(255));
+    if (i % 2) {
+      blobs[i].setVelocity(-1);
+    } else {
+      blobs[i].setVelocity(1);
+    }
   }
 }
 
@@ -80,8 +141,12 @@ void loop() {
   for(int i = 0; i < CHIME_COUNT;i++) {
     chimes[i].step();
     chimes[i].setLEDs(&leds[offset]);
-    offset += chimes[i].length();
+    offset += Chime<CHIME_LENGTH>::Length;
+  }
+  for(int i = 0; i < BLOB_COUNT;i++) {
+    blobs[i].step();
+    blobs[i].setLEDs(leds);
   }
   FastLED.show();
-  delay(10);
+  delay(30);
 }
