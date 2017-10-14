7001a693ba295ae5eaf61d3043e699dd98dd154f
Torrie Fischer <tdfischer@hackerbots.net>
2017-10-13 19:11:34 -0700

    update to run on my bike

--- chimes.ino
+++ chimes.ino
@@ -1,8 +1,8 @@
 #include <FastLED.h>
 
-#define CHIME_COUNT 10
-#define CHIME_LENGTH 14
-#define BLOB_COUNT 6
+#define CHIME_COUNT 7
+#define CHIME_LENGTH 8
+#define BLOB_COUNT 3
 #define LED_NUM (CHIME_COUNT * CHIME_LENGTH)
 
 CRGB leds[LED_NUM];
@@ -49,9 +49,9 @@ public:
   }
   
   void setLEDs(CRGB* leds) {
-      const uint8_t width = 10;
+      const uint8_t width = 25;
       for(uint8_t i = 0;i < width; i++) {
-        int absPos = LED_NUM - (m_pos + i - width / 2);
+        int absPos = LED_NUM - (m_pos + i - (width / 2));
         if (absPos < 0 || absPos >= LED_NUM)
           continue;
         leds[absPos] = blend(leds[absPos], CHSV(m_hue, 200, quadwave8((i / (double)width) * m_brightness)), 128);
@@ -63,6 +63,7 @@ template<int ChimeLength> class Chime {
   uint16_t m_pos;
   uint8_t m_speed;
   uint16_t m_hue;
+  uint16_t m_brightness;
     
 public:
 
@@ -71,6 +72,7 @@ public:
   Chime()
     : m_pos(0),
       m_speed(1),
+      m_brightness(255),
       m_hue(0) {}
   
   void setSpeed(uint8_t i) {
@@ -83,6 +85,10 @@ public:
   
   void setHue(uint8_t i) {
     m_hue = i % 255;
+  }
+
+  void setBrightness(uint8_t i) {
+    m_brightness = i % 255;
   }
   
   void step() {
@@ -105,7 +111,7 @@ public:
         leds[i] = CHSV(0, 0, 0);
       } else {
         uint8_t distance = m_pos - i;
-        uint8_t brightness = quadwave8((ChimeLength / (double)distance) * 255);
+        uint8_t brightness = scale8(quadwave8((ChimeLength / (double)distance) * 255), m_brightness);
         if (brightness <= 0.2)
           brightness = 0;
         leds[i] = CHSV(m_hue, max(200, brightness), brightness);
@@ -118,12 +124,13 @@ Chime<CHIME_LENGTH> chimes[CHIME_COUNT];
 Blob blobs[BLOB_COUNT];
 
 void setup() {
-  FastLED.addLeds<LPD8806>(leds, LED_NUM);
+  FastLED.addLeds<WS2812,11, GBR>(leds, LED_NUM);
   for(int i = 0; i < CHIME_COUNT;i++) {
     chimes[i].setPos(random(Chime<CHIME_LENGTH>::Length * 5));
     chimes[i].setHue(random(255));
-    chimes[i].setSpeed(random(90) + 128);
-  }k
+    chimes[i].setSpeed(random(90) + 138);
+    chimes[i].setBrightness(70);
+  }
   for(int i = 0; i < BLOB_COUNT;i++) {
     blobs[i].setPos(random(LED_NUM));
     blobs[i].setHue(random(255));
@@ -148,5 +155,5 @@ void loop() {
     blobs[i].setLEDs(leds);
   }
   FastLED.show();
-  delay(30);
+  delay(20);
 }
